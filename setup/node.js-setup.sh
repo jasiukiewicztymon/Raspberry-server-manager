2dfa1c56a666d9280914c71370d83cccc7baa2f9
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-01-27 23:42:51 +0100

    Update node.js-setup.sh

--- setup/node.js-setup.sh
+++ setup/node.js-setup.sh
@@ -1,5 +1,5 @@
 #installing stable version
 sudo apt update
-sudo apt install nodejs
-sudo apt install npm
-sudo npm install pm2@latest -g
+sudo apt -y install nodejs
+sudo apt -y install npm
+sudo npm -y install pm2@latest -g
